d6434953d68669fb8e0a605806b981e0e23f97c2
Zidong Wang <zidong.wang@tum.de>
2023-06-18 23:33:19 +0800

    explore part update

---
+++ client/src/components/Pages/Explore/createPost.jsx
@@ -0,0 +1,11 @@
+import React, {useEffect,useState} from "react";
+
+export default function CreatePost(){
+
+    return(
+        <div>
+            <p>sdadqweweasdssssssssssss</p>
+            
+        </div>
+    )
+}
