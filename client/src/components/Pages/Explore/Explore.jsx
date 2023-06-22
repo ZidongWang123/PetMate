96c2e7a4f73731e721cf84a4e7024a803eb8e280
Zidong Wang <zidong.wang@tum.de>
2023-06-22 20:33:26 +0800

    explore update

--- client/src/components/Pages/Explore/Explore.jsx
+++ client/src/components/Pages/Explore/Explore.jsx
@@ -21,26 +21,26 @@ const Explore = () => {
       <div style={{display:"flex"}}>
         <SearchBar/>
         <Link to="/createPost">
-          <Button  variant="contained" 
-            
-            sx={{
-              height:"50px",
-              width:"150px",
-              marginTop: "45px",
-              marginLeft: 2,
-              padding: 2,
-              paddingLeft: 2,
-              paddingRight: 4,
-              borderRadius: "9999px",
-              color: "black",
-              backgroundColor: 'transparent',
-              border: 'gray',
-      
-              ":hover": {
-                color: "white",
-                backgroundColor: orange,
-              },
-            }} >create a post</Button>
+           <Button  variant="contained" 
+          
+          sx={{
+            height:"50px",
+            width:"150px",
+            marginTop: "45px",
+            marginLeft: 2,
+            padding: 2,
+            paddingLeft: 2,
+            paddingRight: 4,
+            borderRadius: "9999px",
+            color: "black",
+            backgroundColor: 'transparent',
+            border: 'gray',
+    
+            ":hover": {
+              color: "white",
+              backgroundColor: orange,
+            },
+          }} >create a post</Button>
         </Link>
        
       </div>
