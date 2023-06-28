b6975fff63659ec644e76b791f4b8c885b6d9c8d
YZ <yuqian.zheng@tum.de>
2023-06-28 10:24:58 +0200

    add brightOrange variable

--- client/src/constant/actionTypes.js
+++ client/src/constant/actionTypes.js
@@ -1,13 +1,14 @@
-export const AUTH = 'AUTH';
-export const LOGOUT = 'LOGOUT';
+export const AUTH = "AUTH";
+export const LOGOUT = "LOGOUT";
 
-export const START_LOADING = 'START_LOADING';
-export const END_LOADING = 'END_LOADING';
+export const START_LOADING = "START_LOADING";
+export const END_LOADING = "END_LOADING";
 
 //font color and background color
-export const paleYellow = 'rgb(245, 242, 236)';
-export const darkPurple = '#393c7c';
-export const orange = '#F0A860';
-export const brightGreen = '#9AC7BF';
-export const brightPurple = '#9C62F2';
-export const darkGray = '#3F3F3F';
+export const paleYellow = "rgb(245, 242, 236)";
+export const darkPurple = "#393c7c";
+export const orange = "#F0A860";
+export const brightGreen = "#9AC7BF";
+export const brightPurple = "#9C62F2";
+export const darkGray = "#3F3F3F";
+export const brightOrange = "#ff8000";
