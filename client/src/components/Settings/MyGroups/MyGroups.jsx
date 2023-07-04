020a5049c986e7204b0d3f89e1039ab9f9e338bb
YZ <yuqian.zheng@tum.de>
2023-07-04 17:02:18 +0200

    my group unit

--- client/src/components/Settings/MyGroups/MyGroups.jsx
+++ client/src/components/Settings/MyGroups/MyGroups.jsx
@@ -1,28 +1,35 @@
-
-
-import React from 'react';
+import React from "react";
 //import useFetch from "../../Widget/DataFetch/useFetch";
 import "./MyGroup.css";
-import MyGroupUnit from './MyGroupUnit';
+import MyGroupUnit from "./MyGroupUnit";
+import { useState, useEffect } from "react";
+import { useWorkoutsContext } from "../../../hooks/useWorkoutsContext";
 
 const MyGroups = () => {
-/*   const {
-    data: groups,
-    isPending,
-    error,
-  } = useFetch("http://localhost:8080/groups");
- */
+  const { workouts: groups, dispatch } = useWorkoutsContext();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchGroups = async () => {
+      const response = await fetch("http://localhost:100/api/groups");
+      const json = await response.json();
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      if (response.ok) {
+        dispatch({ type: "SET_WORKOUTS", payload: json });
+        setIsLoading(false);
+      }
+    };
+    fetchGroups();
+  }, [dispatch]);
+  useEffect(() => {
+    console.log(groups);
+  }, [groups]);
   return (
-    <div className="my-groups">
-      <h1 className="group-title">Welcome back to your Groups you joined</h1> {/* 添加标题，并设置 className */}
-      <div className="fetchGroupList">
-        {/* {error && <div>{error}</div>}
-        {isPending && <div>loading...</div>}
-        {groups && <MyGroupUnit groups={groups} title="groups" />} */}
-      </div>
-
+    <div className="myGroupList">
+      {!isLoading &&
+        groups.map((group) => <MyGroupUnit key={group._id} group={group} />)}
     </div>
-
   );
 };
 
