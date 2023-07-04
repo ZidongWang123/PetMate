7c19b61b69a6ffea497ab4b407f0b841eab1baec
YZ <yuqian.zheng@tum.de>
2023-07-04 17:01:11 +0200

    modify links to single group

--- client/src/components/Pages/Groups/GroupList.js
+++ client/src/components/Pages/Groups/GroupList.js
@@ -7,35 +7,27 @@ import { Link } from "react-router-dom";
 const GroupList = ({ group }) => {
   return (
     <div className="group-preview">
-      <div className="group-avatar">
-        <Link style={{ textDecoration: "none" }} to={`/groups/${group._id}`}>
+      <Link style={{ textDecoration: "none" }} to={`/groups/${group._id}`}>
+        <div className="group-avatar">
           <Avatar alt="Remy Sharp" src={group.imageURL} />
-        </Link>
-      </div>
-      <div className="group-text-review">
-        <div className="group-first-row">
-          <div className="group-name">
-            <Link
-              style={{ textDecoration: "none", color: "inherit" }}
-              to={`/groups/${group._id}`}
-            >
-              {group.title}
-            </Link>
-          </div>
-          <p className="group-creater">Created by:{group.creator}</p>
-          <p className="group-amount">member:{group.numbers}</p>
         </div>
-        <p className="group-intro-text">{group.intro}</p>
-        <p>
-          {group.tags &&
-            group.tags.map((tagItem, index) => (
-              <span key={index} className="tag">
-                #{tagItem}
-              </span>
-            ))}
-        </p>
-      </div>
-
+        <div className="group-text-review">
+          <div className="group-first-row">
+            <div className="group-name">{group.title}</div>
+            <p className="group-creater">Created by:{group.creator}</p>
+            <p className="group-amount">member:{group.numbers}</p>
+          </div>
+          <p className="group-intro-text">{group.intro}</p>
+          <p>
+            {group.tags &&
+              group.tags.map((tagItem, index) => (
+                <span key={index} className="tag">
+                  #{tagItem}
+                </span>
+              ))}
+          </p>
+        </div>
+      </Link>
       <button className="grouplist-button">Join Now</button>
     </div>
   );
