4f374c289a86c79260bb8117b1fb381030ebacdb
Yueting <yueting.guan@tum.de>
2023-07-09 21:46:17 +0200

    delte unneed text

--- client/src/components/Pages/Groups/Post/Post.jsx
+++ client/src/components/Pages/Groups/Post/Post.jsx
@@ -1,11 +1,4 @@
-/*
- * @Author: xiehuan 1208044257@qq.com
- * @Date: 2023-07-05 14:32:19
- * @LastEditors: xiehuan 1208044257@qq.com
- * @LastEditTime: 2023-07-06 20:22:29
- * @FilePath: \prototype\client\src\components\Pages\Groups\Post\Post.jsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
+
 
 import React, { useEffect, useState } from "react";
 import useFetch from "../../../Widget/DataFetch/useFetch";
