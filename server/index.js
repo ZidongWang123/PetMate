c35f1fe6239463a5d4be2bdcf89e5fb6bef0da2a
Yueting <yueting.guan@tum.de>
2023-07-11 18:23:21 +0200

    delete unused

--- server/index.js
+++ server/index.js
@@ -1,12 +1,3 @@
-/*
- * @Author: xiehuan 1208044257@qq.com
- * @Date: 2023-06-30 11:20:53
- * @LastEditors: xiehuan 1208044257@qq.com
- * @LastEditTime: 2023-07-01 14:01:33
- * @FilePath: \prototype\server\index.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-//import all the dependencies
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
