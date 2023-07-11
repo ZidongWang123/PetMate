f0e892049c6e5fb3ded2f23b81ba685ac9b109af
Zidong Wang <zidong.wang@tum.de>
2023-07-11 23:48:13 +0200

    modified:   client/src/api/index.js 	modified:   client/src/components/Pages/Explore/CreatePost/CreatePost.jsx 	modified:   client/src/components/Pages/Explore/Explore.jsx 	modified:   client/src/components/Pages/Explore/PaneContainer/PaneContainer.jsx 	modified:   client/src/components/Pages/Explore/PaneItem/PaneItem.css 	modified:   client/src/components/Pages/Explore/PaneItem/PaneItem.jsx 	modified:   client/src/components/Pages/Explore/PostDetailWindow/PostDetailWindow.css 	modified:   client/src/components/Pages/Explore/PostDetailWindow/PostDetailWindow.jsx 	modified:   client/src/components/Pages/Explore/UserPage/UserPage.jsx 	modified:   client/src/components/Pages/Explore/widget/HeartIcon.jsx 	modified:   server/controllers/exploreController.js 	modified:   server/models/explorePost.js

--- server/routes/explorePost.js
+++ server/routes/explorePost.js
@@ -1,10 +1,13 @@
 import express from 'express';
 
 import { postCreate } from '../controllers/exploreController.js';
-import { getRecommendTags } from '../controllers/exploreController.js';
+import { getRecommendTags, getExplorePosts} from '../controllers/exploreController.js';
+
+
 
 const router = express.Router();
 
 router.post("/createPost", postCreate);
 router.get("/getRecommendTags/:keyword/:where",getRecommendTags)
+router.get("/getPosts",getExplorePosts)
 export default router;
